12cf87a1ee7683c4f39c2d1c72658201ab7297e8
Danna Liu <dliu326@gatech.edu>
2020-04-15 00:03:57 -0500

    add a production build of app

---
+++ A6/source_code/public/precache-manifest.4f98174ab47c5e2cad2365cb3b0ae0c8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "855e24d5b243eb930927a081781353d1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5cd9c76ce50688aa3699",
+    "url": "./static/css/main.4795dca5.chunk.css"
+  },
+  {
+    "revision": "9c17db5001c852edd400",
+    "url": "./static/js/2.e25e926b.chunk.js"
+  },
+  {
+    "revision": "b3924902b6517db7f2b56facf8ba237b",
+    "url": "./static/js/2.e25e926b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cd9c76ce50688aa3699",
+    "url": "./static/js/main.5530b288.chunk.js"
+  },
+  {
+    "revision": "3b0ba3529da5c1cd13d8",
+    "url": "./static/js/runtime-main.7dacd673.js"
+  },
+  {
+    "revision": "60d00ff5a42906b57ca47cb6a4ff37a8",
+    "url": "./static/media/drone.60d00ff5.png"
+  },
+  {
+    "revision": "edfe7c37e35f1a5a582f62ed33614174",
+    "url": "./static/media/stars.edfe7c37.png"
+  },
+  {
+    "revision": "e6daec7386fcd61b870f3dcbbd3d08ab",
+    "url": "./static/media/sun.e6daec73.png"
+  }
+]);
